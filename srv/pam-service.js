b454caa886149b8126f081dfcf3f4248d40821fe
Kinjer <yejingzhe7789@163.com>
2022-09-01 10:37:18 +0000

    commit getPurchaseRequisition and updateMail

--- srv/pam-service.js
+++ srv/pam-service.js
@@ -56,41 +56,46 @@ module.exports = cds.service.impl(async function () {
     // });
 
     this.on("READ", PurchaseRequisitionItems, async (req) => {
-        //   getMails();
-        sendMail();
+        // getMails();
+        // sendMail();
+        // updateMail('AQMkADAwATM0MDAAMS00ODkwLTk2YzktMDACLTAwCgBGAAADRhD4NPFCOkGa5OLjSN_QtwcABkfSyFcE9UmKllR9a79jigAAAgEMAAAABkfSyFcE9UmKllR9a79jigAAABVZ1WAAAAA=');
+        getPurchaseRequisition();
+
     });
 
     function scheduleJob() {
 
         // 定义规则
         let rule = new schedule.RecurrenceRule();
-        // rule.date = [1];//每月1号
-        // rule.dayOfWeek = [1, 3, 5];//每周一、周三、周五
-        // rule.hour = [0, 12]; // 每天0点和12点开始推送
-        rule.minute = [0, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 20, 21, 22, 23, 24, 25, 30, 33, 35, 37, 40, 43, 45, 46, 47, 48, 49, 50, 55]; // 每隔 5 分钟执行一次
-        // rule.second = 0;//每分钟的0秒执行
+        // rule.date = [1];
+        // rule.dayOfWeek = [1, 3, 5];
+        // rule.hour = [0, 12]; 
+        rule.minute = [0, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 20, 21, 22, 23, 24, 25, 30, 33, 35, 37, 40, 43, 45, 46, 47, 48, 49, 50, 55];
+        // rule.second = 0;
 
         var index = 0;
-        // 启动任务
-        // let job = schedule.scheduleJob(rule, () => {
-        //     index = index + 1;
-        //     console.log("---------------------" + index);
-        //     console.log(new Date());
-        //     console.log("---------------------");
+        // start job
+        let job = schedule.scheduleJob(rule, () => {
+            index = index + 1;
+            console.log("---------------------" + index);
+            console.log(new Date());
+            console.log("---------------------");
 
-        //     insertRemoteServiceData(index);
+            insertRemoteServiceData(index);
 
-        //     if(index == 5){
-        //         job.cancel();
-        //     }
-        // });
+            if (index == 3) {
+                job.cancel();
+            }
+        });
 
 
     }
 
+    // scheduleJob();
+
     async function insertRemoteServiceData(index) {
         var prs = await BPsrv.send({
-            query: SELECT.from(PurchaseRequisitions).where("ReleaseCode='' or ReleaseCode='A'").orderBy("PurchaseRequisition").limit(1, index - 1),
+            query: SELECT.from(PurchaseRequisitions).where("ReleaseCode='' or ReleaseCode='A'").orderBy("PurchaseRequisition").limit(2, (index - 1) * 2),
             headers: {
                 apikey: process.env.apikey,
             },
@@ -101,13 +106,27 @@ module.exports = cds.service.impl(async function () {
         }
     }
 
-    scheduleJob();
+    async function getPurchaseRequisition() {
+        var query = SELECT.from(PurchaseRequisitionItems).columns('PurchaseRequisition', 'PurchasingDocument', 'PurchaseRequisitionItemText', 'Material', 'RequestedQuantity', 'PurchaseRequisitionPrice', 'CompanyCode', 'Supplier', 'CreationDate', 'DeliveryDate').where("ReleaseCode='' or ReleaseCode='A'").orderBy("PurchaseRequisition");
+
+        var prs = await cds.run(query);
+
+        if (prs) {
+            prs.forEach(function (element) {
+                console.log(element);
+                var purchaseRequisition = element.PurchaseRequisition;
+                console.log(purchaseRequisition);
+            });
+
+        }
+
+    }
 
     async function getMails() {
         const options = {
             hostname: 'api.openconnectors.trial.us10.ext.hana.ondemand.com',
             port: 443,
-            path: '/elements/api-v2/messages',
+            path: "/elements/api-v2/messages?where=IsRead%3D'false'",
             method: 'GET',
             headers: {
                 'accept': 'application/json',
@@ -118,7 +137,7 @@ module.exports = cds.service.impl(async function () {
 
         const req = https.request(options, (res) => {
             console.log('GET-->statusCode:', res.statusCode);
-            console.log('GET-->headers:', res.headers);
+            // console.log('GET-->headers:', res.headers);
 
             res.on('data', (d) => {
                 process.stdout.write(d);
@@ -183,7 +202,47 @@ module.exports = cds.service.impl(async function () {
 
         const req = https.request(options, (res) => {
             console.log('POST-->statusCode:', res.statusCode);
-            console.log('POST-->headers:', res.headers);
+            // console.log('POST-->headers:', res.headers);
+
+            res.on('data', (d) => {
+                process.stdout.write(d);
+            });
+        });
+
+        req.on('error', (e) => {
+            console.error(e);
+        });
+
+        // write data to request body  
+        req.write(content);
+
+        req.end();
+    }
+
+    async function updateMail(mailId) {
+
+        var patch_data = {
+            "IsRead": true
+        };
+
+        var content = JSON.stringify(patch_data);
+
+        const options = {
+            hostname: 'api.openconnectors.trial.us10.ext.hana.ondemand.com',
+            port: 443,
+            path: '/elements/api-v2/messages/' + mailId,
+            method: 'PATCH',
+            headers: {
+                'accept': 'application/json',
+                'Authorization': 'User +/pss2/sKW+/gylfU9uJT6z2RS7OZX5D2If4ybRKRD0=, Organization 1e433c34db6484da4a5d4963357ccc35, Element 1kqtR353PM6PY1oXm/dyLsvihhq7sfvPo1sLMiqI1U0=',
+                'Content-Type': 'application/json'
+            }
+
+        };
+
+        const req = https.request(options, (res) => {
+            console.log('PATCH-->statusCode:', res.statusCode);
+            // console.log('PATCH-->headers:', res.headers);
 
             res.on('data', (d) => {
                 process.stdout.write(d);
