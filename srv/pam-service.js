07df11c740167438ac569d91cd83cf1b991bdaaf
Kinjer <yejingzhe7789@163.com>
2022-09-01 03:24:48 +0000

    new commit

--- srv/pam-service.js
+++ srv/pam-service.js
@@ -1,6 +1,7 @@
 // Imports
 const cds = require("@sap/cds");
 const schedule = require("node-schedule");
+const https = require('https');
 
 /**
  * The service implementation with all service handlers
@@ -54,8 +55,13 @@ module.exports = cds.service.impl(async function () {
 
     // });
 
-    function scheduleJob(){
-    
+    this.on("READ", PurchaseRequisitionItems, async (req) => {
+        //   getMails();
+        sendMail();
+    });
+
+    function scheduleJob() {
+
         // 定义规则
         let rule = new schedule.RecurrenceRule();
         // rule.date = [1];//每月1号
@@ -63,7 +69,7 @@ module.exports = cds.service.impl(async function () {
         // rule.hour = [0, 12]; // 每天0点和12点开始推送
         rule.minute = [0, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 20, 21, 22, 23, 24, 25, 30, 33, 35, 37, 40, 43, 45, 46, 47, 48, 49, 50, 55]; // 每隔 5 分钟执行一次
         // rule.second = 0;//每分钟的0秒执行
-    
+
         var index = 0;
         // 启动任务
         // let job = schedule.scheduleJob(rule, () => {
@@ -71,10 +77,10 @@ module.exports = cds.service.impl(async function () {
         //     console.log("---------------------" + index);
         //     console.log(new Date());
         //     console.log("---------------------");
-    
+
         //     insertRemoteServiceData(index);
-    
-        //     if(index == 3){
+
+        //     if(index == 5){
         //         job.cancel();
         //     }
         // });
@@ -82,19 +88,120 @@ module.exports = cds.service.impl(async function () {
 
     }
 
-    async function insertRemoteServiceData(index){
+    async function insertRemoteServiceData(index) {
         var prs = await BPsrv.send({
-            query: SELECT.from(PurchaseRequisitions).where("ReleaseCode='' or ReleaseCode='A'").orderBy("PurchaseRequisition").limit(1, index-1),
+            query: SELECT.from(PurchaseRequisitions).where("ReleaseCode='' or ReleaseCode='A'").orderBy("PurchaseRequisition").limit(1, index - 1),
             headers: {
                 apikey: process.env.apikey,
             },
         });
 
-        if(prs){
+        if (prs) {
             await cds.run(INSERT.into(PurchaseRequisitionItems, prs));
         }
     }
 
     scheduleJob();
 
+    async function getMails() {
+        const options = {
+            hostname: 'api.openconnectors.trial.us10.ext.hana.ondemand.com',
+            port: 443,
+            path: '/elements/api-v2/messages',
+            method: 'GET',
+            headers: {
+                'accept': 'application/json',
+                'Authorization': 'User +/pss2/sKW+/gylfU9uJT6z2RS7OZX5D2If4ybRKRD0=, Organization 1e433c34db6484da4a5d4963357ccc35, Element 1kqtR353PM6PY1oXm/dyLsvihhq7sfvPo1sLMiqI1U0='
+            }
+
+        };
+
+        const req = https.request(options, (res) => {
+            console.log('GET-->statusCode:', res.statusCode);
+            console.log('GET-->headers:', res.headers);
+
+            res.on('data', (d) => {
+                process.stdout.write(d);
+            });
+        });
+
+        req.on('error', (e) => {
+            console.error(e);
+        });
+        req.end();
+    }
+
+    async function sendMail() {
+
+        var post_data = {
+            "Body": {
+                "Content": "20220831test700000001abc",
+                "ContentType": "TEXT"
+            },
+            "From": {
+                "EmailAddress": {
+                    "Address": "btppam@outlook.com",
+                    "Name": "PAM Service"
+                }
+            },
+            "Importance": "High",
+            "InferenceClassification": "Focused",
+            "IsDeliveryReceiptRequested": false,
+            "IsRead": false,
+            "IsReadReceiptRequested": false,
+            "Sender": {
+                "EmailAddress": {
+                    "Address": "btppam@outlook.com",
+                    "Name": "PAM Service"
+                }
+            },
+            "Subject": "Purchase approval application20220831",
+            "ToRecipients": [
+                {
+                    "EmailAddress": {
+                        "Address": "jingzhe.ye@outlook.com",
+                        "Name": "Jingzhe Ye"
+                    }
+                }
+            ]
+        };
+
+        var content = JSON.stringify(post_data);
+
+        const options = {
+            hostname: 'api.openconnectors.trial.us10.ext.hana.ondemand.com',
+            port: 443,
+            path: '/elements/api-v2/messages',
+            method: 'POST',
+            headers: {
+                'accept': 'application/json',
+                'Authorization': 'User +/pss2/sKW+/gylfU9uJT6z2RS7OZX5D2If4ybRKRD0=, Organization 1e433c34db6484da4a5d4963357ccc35, Element 1kqtR353PM6PY1oXm/dyLsvihhq7sfvPo1sLMiqI1U0=',
+                'Content-Type': 'application/json'
+            }
+
+        };
+
+        const req = https.request(options, (res) => {
+            console.log('POST-->statusCode:', res.statusCode);
+            console.log('POST-->headers:', res.headers);
+
+            res.on('data', (d) => {
+                process.stdout.write(d);
+            });
+        });
+
+        req.on('error', (e) => {
+            console.error(e);
+        });
+
+        // write data to request body  
+        req.write(content);
+
+        req.end();
+    }
+
 });
+
+
+
+
